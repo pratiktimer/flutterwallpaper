e14c9ae1bdd9acb8e24304843bc77e77b49ad4df
praban <pbanawalkar2@gmail.com>
2023-07-22 18:59:38 +0530

    clean code setup started

--- lib/main.dart
+++ lib/main.dart
@@ -1,124 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'dart:async';
-import 'package:flutterwallpaper/video_cell.dart';
-import 'package:flutter/services.dart';
+import 'package:flutterwallpaper/core/resources/data_state.dart';
+import 'package:flutterwallpaper/data/data_sources/remote/wallpaper_service.dart';
+import 'package:flutterwallpaper/domain/repository/wallpaper_repository.dart';
+import 'package:flutterwallpaper/firebase_options.dart';
+import 'package:flutterwallpaper/injection_container.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'data/repository/wallpaper_repository_impl.dart';
+import 'domain/entities/wallpaper.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await initializeDependencies();
+  runApp(const MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  int count;
-  static int l = 0;
-  StreamSubscription<QuerySnapshot> subscription2;
-  List<DocumentSnapshot> wallpaperlist2;
-  CollectionReference collectionReference2 =
-      Firestore.instance.collection("wallpapers");
-  StreamSubscription<QuerySnapshot> subscription;
-  List<DocumentSnapshot> wallpaperlist;
-  CollectionReference collectionReference =
-      Firestore.instance.collection("wallpapers").document("1").collection("1");
-  var _isLoading = true;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  _fetchData() async {
-    subscription = collectionReference.snapshots().listen((datasnapshot) {
-      setState(() {
-        wallpaperlist = datasnapshot.documents;
-        _isLoading = false;
-      });
-    });
-  }
-
-  _fetchData0() async {
-    subscription2 = collectionReference2.snapshots().listen((datasnapshot) {
-      setState(() {
-        wallpaperlist2 = datasnapshot.documents;
-      });
-    });
-    _fetchData();
-  }
-
-  _fetchData2() {
-    count = wallpaperlist2.length;
-    l = l + 1;
-    var p = "$l";
-    if (count > l) {
-      collectionReference =
-          Firestore.instance.collection("wallpapers").document(p).collection(p);
-    } else {
-      l = 0;
-      collectionReference =
-          Firestore.instance.collection("wallpapers").document(p).collection(p);
-    }
-    _fetchData();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-    _fetchData0();
-  }
-
-  @override
-  void dispose() {
-    subscription?.cancel();
-    subscription2?.cancel();
-    super.dispose();
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.landscapeRight,
-      DeviceOrientation.landscapeLeft,
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-  }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: new SafeArea(
-        child: Scaffold(
-          //backgroundColor: const Color(0xFF010101).withOpacity(1.0),
-          appBar: new AppBar(
-            centerTitle: true,
-            title: IconButton(
-              icon: new Icon(Icons.navigate_next),
-              onPressed: () {
-                print("Reloading...");
-                _fetchData2();
-              },
-            ),
-            backgroundColor: const Color(0xFF010101).withOpacity(1.0),
-          ),
-          body: Center(
-            child: _isLoading
-                ? CircularProgressIndicator()
-                : StaggeredGridView.countBuilder(
-                    padding: EdgeInsets.all(8.0),
-                    crossAxisCount: 4,
-                    itemCount: this.wallpaperlist != null
-                        ? this.wallpaperlist.length
-                        : 0,
-                    itemBuilder: (context, i) {
-                      String imgurl = wallpaperlist[i].data['url'];
-                      return VideoCell(imgurl);
-                    },
-                    staggeredTileBuilder: (i) =>
-                        StaggeredTile.count(2, i.isEven ? 2 : 3),
-                    mainAxisSpacing: 8.0,
-                    crossAxisSpacing: 8.0,
-                  ),
-          ),
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
+        home: Scaffold(body: Center(child: WallpaperListWidget())),
       ),
     );
   }
