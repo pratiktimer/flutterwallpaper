6c2430d847862ec148a46af4bcc30c4ea1c5ba62
praban <pbanawalkar2@gmail.com>
2023-11-11 19:58:19 +0530

    build number incrementd for playstore

---
+++ lib/data/models/category_name_dto.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'category_name_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CategoryNameDTO _$CategoryNameDTOFromJson(Map<String, dynamic> json) =>
+    CategoryNameDTO(
+      name: json['name'],
+    );
+
+Map<String, dynamic> _$CategoryNameDTOToJson(CategoryNameDTO instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+    };
---
+++ lib/data/models/image_color_category_dto.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'image_color_category_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ImageColorCategoryDTO _$ImageColorCategoryDTOFromJson(
+        Map<String, dynamic> json) =>
+    ImageColorCategoryDTO(
+      name: json['name'],
+      hexValue: json['hexValue'],
+      imageUrl: json['imageUrl'],
+    );
+
+Map<String, dynamic> _$ImageColorCategoryDTOToJson(
+        ImageColorCategoryDTO instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'hexValue': instance.hexValue,
+      'imageUrl': instance.imageUrl,
+    };
