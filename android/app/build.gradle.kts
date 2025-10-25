0c1e75e045a847e4810a346fb9cd6b6148ab1078
praban <pbanawalkar2@gmail.com>
2025-10-25 22:16:29 +0530

    latest build for playstore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode =30
+        versionName = "3.0.0"
     }
     signingConfigs {
         create("release") {
